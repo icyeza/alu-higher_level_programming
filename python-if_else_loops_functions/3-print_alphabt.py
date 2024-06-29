a91a63214d3c8f45deabb70414c0e2e9f7fe1ff6
Lorita <sesameicyeza@gmail.com>
2024-06-29 13:48:30 -0700

    alphabet

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for alpha_letters in range(ord('a'), ord('z')+1):
-    if alpha_letters == 'e' or alpha_letters == 'q':
-       continue
-    print("{:c}".format(alpha_letters), end="")
+
+for i in range(ord('a'), ord('z')+1):
+    if i != ord('q') and i != ord('e'):
+        print("{}".format("".join(chr(i))), end="")
+    else:
+        pass
