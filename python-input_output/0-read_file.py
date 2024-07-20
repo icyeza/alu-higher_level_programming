576a7f7be04bab2e55616b2f8d3e80f69ccdcc56
Lorita <sesameicyeza@gmail.com>
2024-07-20 13:03:03 -0700

    ok

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """
 Simple Python file with a fuction that open files
@@ -9,4 +8,3 @@ def read_file(filename=""):
     """ Function That opens files with UTF8 encoding """
     with open(filename, 'r', encoding="utf-8") as f:
         print(f.read(), end="")
-
