36db25be71cd6c2b2cbde372fa961a140b608fb2
Icyeza <sesameicyeza@gmail.com>
2024-07-30 19:04:23 +0000

    ok

--- 0-body_size.sh
+++ 0-body_size.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-# display Content-Lengthfter editing, stage the changes
-git add 0-body_size.sh
-
-# Commit the changes
-git commit -m "Updated 0-body_size.sh"
-
-# Push the changes to the remote repository
-git push origin <branch-name>  # Replace <branch-name> with your branch name
-
+# display Content-Length
 curl -sI "$1" | grep 'Content-Length' | cut -d " " -f2
