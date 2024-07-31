b1088577bf9fde457295d207b09a762fdff1957a
icyeza <sesameicyeza@gmail.com>
2024-07-31 21:58:39 +0000

    ok

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-''' OK '''
+'''OK'''
 
 
 class Base:
-	''' OK '''
+	'''OK'''
 	__nb_objects = 0
+
 	def __init__(self, id=None):
+         '''class instructor'''
 		if id is not None:
 			self.id = id
 		else:  
-			__nb_objects+=1
-			self.id=  __nb_objects  
+		    	Base.__nb_objects+=1
+			self.id= Base.__nb_objects  
