a21413f7ced16f318f4a103f7bf8532045ff3082
icyeza <sesameicyeza@gmail.com>
2024-07-31 21:00:48 +0000

    ok

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
+"""
+Documentation
+Fetches data from the url using
+the urllib module in python
+"""
+
 import urllib.request
 
-# Define the URL
-url = "https://alu-intranet.hbtn.io/status"
+url = 'https://intranet.hbtn.io/status'
+if url.startswith('https://'):
+    url = 'https://alu-intranet.hbtn.io/status'
 
-# Fetch and display the content
-with urllib.request.urlopen(url) as response:
-    body = response.read()
-
-print("Body response:")
-print("\t- type: {}".format(type(body)))
-print("\t- content: {}".format(body))
-print("\t- utf8 content: {}".format(body.decode('utf-8')))
+if __name__ == '__main__':
+    with urllib.request.urlopen(url) as f:
+        content = f.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
