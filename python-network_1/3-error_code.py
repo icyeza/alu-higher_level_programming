3039a472a285616a58a366d812da143fcf00184b
icyeza <sesameicyeza@gmail.com>
2024-08-01 00:09:08 +0000

    ok

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""call content"""
+"""I documented you"""
 
 import urllib.request
 import urllib.error
 import sys
 
 if __name__ == '__main__':
-    """"Message"""
+    """"Documented"""
     url = sys.argv[1]
     req = urllib.request.Request(url)
     try:
         with urllib.request.urlopen(req) as response:
-            data = response.read()
-            print("{}".format(data.decode("utf-8")))
+            content = response.read()
+            print("{}".format(content.decode("utf-8")))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
     except urllib.error.URLError as e:
-        print(e.reason)
+        print(e.reason)print(e.reason)
